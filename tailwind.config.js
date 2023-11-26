7144b1b45f89cbede1c69020fae4583bbf8febfc
mjamecny <jamecny.m@tutanota.com>
2023-11-26 18:04:24 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     fontFamily: {
       sans: ["Raleway", "sans-serif"],
     },
+    colors: {
+      orange: "hsl(6, 100%, 80%)",
+      magenta: "hsl(335, 100%, 65%)",
+      paleBlue: "hsl(243, 100%, 93%)",
+      grayishBlue: "hsl(229, 7%, 55%)",
+      darkBlue: "hsl(228, 56%, 26%)",
+      veryDarkBlue: "hsl(229, 57%, 11%)",
+    },
   },
   plugins: [],
 }
